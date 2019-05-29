e3c85087741159e5724a8a0d293e1665f88dd6c6
Filip <f.cieminski@gmail.com>
2019-05-29 23:05:27 +0200

    controller created

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::resource('/crud', 'CrudController', [
+    'except' => ['edit', 'show', 'store']
+]);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
